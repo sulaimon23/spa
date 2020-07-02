3fff12fae81ef2f6a4bb4bd2f44850eaae7f65a1
sulaimon23 <tsulaimon21@yahoo.com>
2020-07-02 22:50:43 +0100

    pet

--- js/spa.js
+++ js/spa.js
@@ -1,8 +1,3 @@
 setTimeout(function () {
     $('.inn').show().addClass('animated fadeInUp');
 }, 1600);
-
-setTimeout(function () {
-    $('.inl').show().addClass('animated fadeInUp');
-}, 2200);
-
